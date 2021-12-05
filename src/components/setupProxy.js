37efde1744cb569b585310e55abce571356ca21a
Ashutosh <ashupotdar2001@gmail.com>
2021-12-05 13:44:54 +0530

    Resolved cors errors

--- src/components/setupProxy.js
+++ src/components/setupProxy.js
@@ -1,12 +1,12 @@
-// const proxy = require('http-proxy-middleware')
+const proxy = require('http-proxy-middleware')
 
-// module.exports = function(app){
-//     app.use(
-//         proxy("/historics/",{
-//             target:'https://v2.api.forex',
-//             changeOrigin:true
-//         }
+module.exports = function(app){
+    app.use(
+        proxy("/historics/",{
+            target:'https://v2.api.forex',
+            changeOrigin:true
+        }
 
-//         )
-//     )
-// }
+        )
+    )
+}
